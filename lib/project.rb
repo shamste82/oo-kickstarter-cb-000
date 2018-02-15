a526f5fc8c5fb1bdf948467a93e7359ff69dec21
Steffen Hammelow-Berg <shammelow@gmail.com>
2018-02-15 13:43:39 +0000

    automatically backed up by learn

---
+++ lib/project.rb
@@ -0,0 +1,12 @@
+class Project
+  attr_accessor :title, :backers
+
+  def initialize(title)
+    @title = title
+    @backers = Array.new
+  end
+
+  def add_backer(backer)
+      @backers.push(backer)
+  end
+end
